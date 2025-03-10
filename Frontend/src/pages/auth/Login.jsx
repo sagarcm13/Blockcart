28fe993683315ad050cbe12626c9501c0e8f1688
sagarcm13 <sagarcm13@gmail.com>
2025-03-10 23:40:40 +0530

    user type defined

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -9,6 +9,7 @@ export default function Login({ onLogin }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+    const [user, setUser] = useState('buyer');
     const mutation = useMutation((data) =>
         axiosClient.post("/login", data), {
         onSuccess: (response) => {
@@ -37,7 +38,7 @@ export default function Login({ onLogin }) {
         setPassword('');
     };
     if (mutation.isLoading) {
-        return <span>Submitting...</span>;
+        return <span className='text-white'>Submitting...</span>;
     }
     if (mutation.isError) {
         return <div className='text-center text-white text-2xl'>Error: {mutation.error.response.data.message}</div>;
@@ -46,12 +47,27 @@ export default function Login({ onLogin }) {
         <>
             <div className='flex justify-center mt-20 '>
                 <form onSubmit={submitData} className='text-white p-10 border-2 border-white flex justify-center flex-col space-y-6 bg-white rounded-2xl'>
-                    <input type="email" className='text-black rounded-2xl p-3' placeholder='Email' name="" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <div className='flex'>
-                        <input type="password" className='text-black rounded-2xl p-3 ' placeholder='Password' name="" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <div className='flex justify-center text-black font-bold'><div>Login</div></div>
+                    <div className='flex justify-center space-x-6'>
+                        <div className='flex flex-col'>
+                            <label className='text-black font-normal'>Buyer</label>
+                            <input type="radio" name="drone" value="buyer" checked={user === "buyer"} onChange={() => setUser('buyer')} />
+                        </div>
+                        <div className='flex flex-col'>
+                            <label className='text-black font-normal'>Seller</label>
+                            <input type="radio" name="drone" value="seller" checked={user === "seller"} onChange={() => setUser('seller')} />
+                        </div>
+                        <div className='flex flex-col'>
+                            <label className='text-black font-normal'>Logistics</label>
+                            <input type="radio" name="drone" value="logistics" checked={user === "logistics"} onChange={() => setUser('logistics')} />
+                        </div>
+                    </div>
+                    <input type="email" className='text-black rounded-2xl p-3 border-2 focus:outline-none' placeholder='Email' name="" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <div className='flex border-2 rounded-2xl'>
+                        <input type="password" className='text-black rounded-2xl p-3 focus:outline-none' placeholder='Password' name="" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                         <img src="https://cdn-icons-png.flaticon.com/128/10910/10910442.png" className='py-1 m-1' id='img' onClick={view} width={30} height={5} alt="" />
                     </div>
-                    <input type="submit" className='bg-blue-600 rounded-2xl p-1' value="Submit" />
+                    <input type="submit" className='bg-blue-600 rounded-2xl p-1 cursor-pointer' value="Submit" />
                     <div className='text-black text-xs'> Don&apos;t have an account &nbsp;<Link to={`/sign_up`} className='text-blue-600'>sign up</Link>
                     </div>
                 </form>
--- Frontend/src/pages/auth/SignUp.jsx
+++ Frontend/src/pages/auth/SignUp.jsx
@@ -6,61 +6,74 @@ import { useNavigate } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 export default function SignUp({ onLogin }) {
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [cpassword, setCPassword] = useState('');
-  const navigate = useNavigate();
-  const mutation = useMutation((data) =>
-    axiosClient.post("/sign_up", data), {
-    onSuccess: (response) => {
-      console.log('Success:', response.data);
-      localStorage.setItem("token", response.data.token);
-      onLogin();
-      navigate('/')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [cpassword, setCPassword] = useState('');
+    const [user, setUser] = useState('buyer');
+    const navigate = useNavigate();
+    const mutation = useMutation((data) =>
+        axiosClient.post("/sign_up", data), {
+        onSuccess: (response) => {
+            console.log('Success:', response.data);
+            localStorage.setItem("token", response.data.token);
+            onLogin();
+            navigate('/')
+        }
     }
-  }
-  );
-  const view = (i, pas) => {
-    let t = document.getElementById(pas);
-    let img = document.getElementById(i);
-    if (t.type === 'password') {
-      t.type = 'text';
-      img.src = 'https://cdn-icons-png.flaticon.com/128/11502/11502541.png';
-    } else {
-      t.type = 'password';
-      img.src = 'https://cdn-icons-png.flaticon.com/128/10910/10910442.png';
+    );
+    const view = (i, pas) => {
+        let t = document.getElementById(pas);
+        let img = document.getElementById(i);
+        if (t.type === 'password') {
+            t.type = 'text';
+            img.src = 'https://cdn-icons-png.flaticon.com/128/11502/11502541.png';
+        } else {
+            t.type = 'password';
+            img.src = 'https://cdn-icons-png.flaticon.com/128/10910/10910442.png';
+        }
     }
-  }
-  const submitData = async (event) => {
-    event.preventDefault();
-    if (password !== cpassword) {
-      alert("password and confirm password mismatch")
-      return
-    }
-    mutation.mutate({ userName, email, password });
-    setEmail('');
-    setUserName('');
-    setPassword('');
-    setCPassword('');
-  };
-  return (
-    <div className='flex justify-center mt-20 '>
-      <form onSubmit={submitData} className='text-white p-10 border-2 border-white flex justify-center flex-col space-y-6 bg-white rounded-2xl'>
-        <input type="text" className='text-black  rounded-2xl p-3' name="" placeholder='Username' id="name" value={userName} onChange={(e) => setUserName(e.target.value)} />
-        <input type="email" className='text-black rounded-2xl p-3' placeholder='Email' name="" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <div className='flex'>
-          <input type="password" className='text-black rounded-2xl p-3 ' placeholder='Password' name="" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <img src="https://cdn-icons-png.flaticon.com/128/10910/10910442.png" className='py-1 m-1' id='img1' onClick={() => view('img1', 'password')} width={30} height={5} alt="" />
+    const submitData = async (event) => {
+        event.preventDefault();
+        if (password !== cpassword) {
+            alert("password and confirm password mismatch")
+            return
+        }
+        mutation.mutate({ email, password });
+        setEmail('');
+        setPassword('');
+        setCPassword('');
+    };
+    return (
+        <div className='flex justify-center mt-20 '>
+            <form onSubmit={submitData} className='text-white p-10 border-2 border-white flex justify-center flex-col space-y-6 bg-white rounded-2xl'>
+                <div className='flex justify-center text-black font-bold'><div>Sign Up</div></div>
+                <div className='flex justify-center space-x-6'>
+                    <div className='flex flex-col'>
+                        <label className='text-black font-normal'>Buyer</label>
+                        <input type="radio" name="drone" value="buyer" checked={user === "buyer"} onChange={() => setUser('buyer')} />
+                    </div>
+                    <div className='flex flex-col'>
+                        <label className='text-black font-normal'>Seller</label>
+                        <input type="radio" name="drone" value="seller" checked={user === "seller"} onChange={() => setUser('seller')} />
+                    </div>
+                    <div className='flex flex-col'>
+                        <label className='text-black font-normal'>Logistics</label>
+                        <input type="radio" name="drone" value="logistics" checked={user === "logistics"} onChange={() => setUser('logistics')} />
+                    </div>
+                </div>
+                <input type="email" className='text-black rounded-2xl p-3' placeholder='Email' name="" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <div className='flex'>
+                    <input type="password" className='text-black rounded-2xl p-3 ' placeholder='Password' name="" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <img src="https://cdn-icons-png.flaticon.com/128/10910/10910442.png" className='py-1 m-1' id='img1' onClick={() => view('img1', 'password')} width={30} height={5} alt="" />
+                </div>
+                <div className='flex'>
+                    <input type="password" className='text-black rounded-2xl p-3' placeholder='Confirm Password' name="" id="cpassword" value={cpassword} onChange={(e) => setCPassword(e.target.value)} />
+                    <img src="https://cdn-icons-png.flaticon.com/128/10910/10910442.png" className='py-1 m-1' id='img2' onClick={() => view('img2', 'cpassword')} width={30} height={5} alt="" />
+                </div>
+                <input type="submit" className='bg-blue-600 rounded-2xl p-1' value="Submit" />
+                <div className='text-black text-xs'>already have an account &nbsp;<Link to={`/login`} className='text-blue-600'>sign in</Link>
+                </div>
+            </form>
         </div>
-        <div className='flex'>
-          <input type="password" className='text-black rounded-2xl p-3' placeholder='Confirm Password' name="" id="cpassword" value={cpassword} onChange={(e) => setCPassword(e.target.value)} />
-          <img src="https://cdn-icons-png.flaticon.com/128/10910/10910442.png" className='py-1 m-1' id='img2' onClick={() => view('img2', 'cpassword')} width={30} height={5} alt="" />
-        </div>
-        <input type="submit" className='bg-blue-600 rounded-2xl p-1' value="Submit" />
-        <div className='text-black text-xs'>already have an account &nbsp;<Link to={`/login`} className='text-blue-600'>sign in</Link>
-        </div>
-      </form>
-    </div>
-  )
+    )
 }
